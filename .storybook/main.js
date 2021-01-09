201e4c32f97b2dd57c445cdc563d66bb34f3f711
IN22915301 <priyanka.guha@tesco.com>
2021-01-09 19:45:26 +0530

    storybook packages composition

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "refs": {
+    '@cstorybook/atoms': {
+       title: "Atoms",
+       url: "docs/storybook/atoms.json"
+    },
+    '@cstorybook/molecules': {
+      title: "Molecules",
+      url: "docs/storybook/molecules.json"
+   }
+  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
